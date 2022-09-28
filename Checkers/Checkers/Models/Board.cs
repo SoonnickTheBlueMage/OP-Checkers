12583ffc3e39df31b35447c55aad0ec2693d4c7f
Андриянов Александр <alexandri2003@gmail.com>
2022-09-28 13:29:51 +0300

    Добавил класс Figure, начал класс Board

---
+++ Checkers/Checkers/Models/Board.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Checkers.Models;
+
+public class Board
+{
+    private List<List<Figure?>> _board;
+
+    public Board()
+    {
+        for (var i = 0; i < 8; i++)
+        {
+            _board.Add(new List<Figure?>());
+            
+            for (var j = 0; j < 8; j++)
+            {
+                _board
+            }
+        }
+    }
+}
---
+++ Checkers/Checkers/Models/Figure.cs
@@ -0,0 +1,41 @@
+﻿namespace Checkers.Models;
+
+public enum Color
+{
+    White,
+    Black
+}
+
+public enum Status
+{
+    Checker,
+    Queen,
+    Out
+}
+
+public class Figure
+{
+    private readonly Color _color;
+    private Status _status;
+    
+    public Figure(Color color)
+    {
+        _color = color;
+        _status = Status.Checker;
+    }
+
+    public Color GetColor()
+    {
+        return this._color;
+    }
+
+    public Status GetStatus()
+    {
+        return this._status;
+    }
+
+    public void SetStatus(Status status)
+    {
+        this._status = status;
+    }
+}
